84f18cc9abc6385a616428d26764106a706c2476
Yaroslav Bogomolov <bogomolov200@yandex.ru>
2024-03-18 17:01:06 +0500

    add 2

--- filmography/src/main/java/testgroup/Main.java
+++ filmography/src/main/java/testgroup/Main.java
@@ -1,7 +1,11 @@
 package testgroup;
 
+import testgroup.filmography.dao.FilmDAOImpl;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+
     }
 }
