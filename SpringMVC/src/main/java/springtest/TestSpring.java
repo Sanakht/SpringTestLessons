e98405a2f4b8d91fc8410762f8355f021e19ee97
Yaroslav Bogomolov <bogomolov200@yandex.ru>
2024-04-10 22:44:11 +0500

    DI

--- SpringMVC/src/main/java/springtest/TestSpring.java
+++ SpringMVC/src/main/java/springtest/TestSpring.java
@@ -6,10 +6,13 @@ public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("ApplicationContext.xml");
 
-        Film film = context.getBean("filmBean", Film.class);
+        // Film film = context.getBean("filmBean", Film.class);
 
-        FilmPlayer filmPlayer = new FilmPlayer(film);
+        //FilmPlayer filmPlayer = new FilmPlayer(film);
 
+        //filmPlayer.playFilm();
+
+        FilmPlayer filmPlayer = context.getBean("filmPlayer", FilmPlayer.class);
         filmPlayer.playFilm();
 
         context.close();
