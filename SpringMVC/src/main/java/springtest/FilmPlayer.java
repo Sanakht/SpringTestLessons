bb833ddcba55242d8e0cf4a928e1d0f452f31d6e
Yaroslav Bogomolov <bogomolov200@yandex.ru>
2024-04-17 01:04:48 +0500

    Test 12

--- SpringMVC/src/main/java/springtest/FilmPlayer.java
+++ SpringMVC/src/main/java/springtest/FilmPlayer.java
@@ -1,14 +1,18 @@
 package springtest;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Component
+@Scope("singleton")
 public class FilmPlayer {
     @Autowired
     @Qualifier("classicalFilm")
@@ -19,6 +23,15 @@ public class FilmPlayer {
 
     @Value("${filmPlayer.value}")
     private int value;
+
+    @PostConstruct
+    public void doMyInit(){
+        System.out.println("Метод срабатывающий при инициализации объекта!");
+    }
+    @PreDestroy
+    public void doMyDestroy(){
+        System.out.println("Метод срабатывающий при уничтожение объекта!");
+    }
 
     public String getName() {
         return name;
--- SpringMVC/src/main/java/springtest/TestSpring.java
+++ SpringMVC/src/main/java/springtest/TestSpring.java
@@ -6,8 +6,15 @@ public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("ApplicationContext.xml");
 
+        FilmPlayer filmPlayer1 = context.getBean("filmPlayer", FilmPlayer.class);
+
+        FilmPlayer filmPlayer2 = context.getBean("filmPlayer", FilmPlayer.class);
+
+        System.out.println(filmPlayer1 == filmPlayer2);
+
 
         Computer computer = context.getBean("computer", Computer.class);
+
 
         System.out.println(computer.getFilmPlayer().getName());
         System.out.println(computer.getFilmPlayer().getValue());
