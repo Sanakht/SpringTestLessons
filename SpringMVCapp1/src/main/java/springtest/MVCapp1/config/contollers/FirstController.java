13f2f2abdffdeefa03669715ec64ed71f009ad4a
Yaroslav Bogomolov <bogomolov200@yandex.ru>
2024-04-24 16:35:19 +0500

    1

--- SpringMVCapp1/src/main/java/springtest/MVCapp1/config/contollers/FirstController.java
+++ SpringMVCapp1/src/main/java/springtest/MVCapp1/config/contollers/FirstController.java
@@ -1,19 +1,28 @@
 package springtest.MVCapp1.config.contollers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Repository;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/first")
 public class FirstController {
     @GetMapping("/hello")
-    public String helloPage(){
+    public String helloPage(@RequestParam(value = "name", required = false)  String name,
+                            @RequestParam(value = "surname", required = false) String surname,
+                            Model model){
+
+
+        //System.out.println("Hello, "  + name + " " + surname);
+        model.addAttribute("message", "Hello, "  + name + " " + surname);
+
         return "first/hello";
     }
     @GetMapping("/goodbye")
     public String goodByPage(){
+
         return "first/goodbye";
     }
 }
