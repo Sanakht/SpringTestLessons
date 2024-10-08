252b4c15f246786aba095c65f626dbca14cd37b5
Yaroslav Bogomolov <bogomolov200@yandex.ru>
2024-10-08 22:42:15 +0500

    1

--- Spring-app1/src/main/java/ru/springtest/springcourse/TestSpring.java
+++ Spring-app1/src/main/java/ru/springtest/springcourse/TestSpring.java
@@ -8,11 +8,13 @@ public class TestSpring {
                 "applicationContext.xml"
         );
 
-        Music music = context.getBean("MusicBean", Music.class);
 
-        MusicPlayer musicPlayer = new MusicPlayer(music);
+
+        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
 
         musicPlayer.playMusic();
+        System.out.println(musicPlayer.getName());
+        System.out.println(musicPlayer.getVolume());
 
         context.close();
     }
